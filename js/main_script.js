9b5f91a33c4fd167add91481ab6c18124122359f
forjos <forjos@mail.ru>
2019-06-14 17:31:45 +0700

    Update main_script.js

--- js/main_script.js
+++ js/main_script.js
@@ -47,3 +47,37 @@ $('.btn_app_replay').click(function(event){
     $(".plus").addClass("plus_active");
     $('.btn_add_amount').removeClass('add_amount_active');
 });
+
+
+/* menu */
+$( '<div class="hamburger_box"><div class="line-hamburger-box"><div class="line-hamburger1"></div><div class="line-hamburger2"></div><div class="line-hamburger3"></div><p class="close-text">закрыть</p></div></div>' ).appendTo( '.t199__mmenu' );
+$('.line-hamburger-box').click(function(event){
+    $(this).toggleClass('activ_menu');
+    $('nav.t199__menu').toggleClass('activ_menu');
+});
+
+
+
+
+/* fixed меню */
+$(document).ready(function() {
+var lastScrollTop = 100;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){   
+     $('.t199__header').addClass('fixed');
+   } else {
+     $('.t199__header').removeClass('fixed');
+   }
+   lastScrollTop = st;
+});
+ $(window).scroll(function() {
+ if($(this).scrollTop() >= 100) {
+ $('.t199__header').addClass('scroll');
+ }
+ else{
+ $('.t199__header').removeClass('scroll');
+ }
+ });
+});
+/* fixed меню end */
