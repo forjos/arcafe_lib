1d6120c578e97271ef6184151097d47fabe70572
forjos <forjos@mail.ru>
2019-06-14 14:47:00 +0700

    remove 21

--- js/model.js
+++ js/model.js
@@ -51,9 +51,9 @@ function main() {
 
   {
     const objLoader = new THREE.OBJLoader2();
-    objLoader.loadMtl('https://forjos.github.io/arcafe_lib/model/21.mtl', null, (materials) => {
+    objLoader.loadMtl('https://forjos.github.io/arcafe_lib/model/16.mtl', null, (materials) => {
       objLoader.setMaterials(materials);
-      objLoader.load('https://forjos.github.io/arcafe_lib/model/21.obj', (event) => {
+      objLoader.load('https://forjos.github.io/arcafe_lib/model/16.obg', (event) => {
         const root = event.detail.loaderRootNode;
         scene.add(root);
       });
