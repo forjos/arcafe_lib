3159713e332f0b994cfb3c19572c97b18e27b415
forjos <forjos@mail.ru>
2019-09-04 01:56:58 +0700

    Update model2.js

--- js/model2.js
+++ js/model2.js
@@ -1,61 +1,61 @@
 // Three.js - Load .OBJ and .MTL file
 // from https://threejsfundamentals.org/threejs/threejs-load-obj-materials.html
 
-   'use strict';
+  'use strict';
 
- /* global THREE */
+/* global THREE */
 
- function main() {
-  const canvas = document.querySelector('#c');
+function main() {
+  const canvas = document.querySelector('#c2');
   const renderer = new THREE.WebGLRenderer({canvas, antialias: true, alpha: true});
-
-   const fov = 45;
+  
+  const fov = 45;
   const aspect = 2;  // the canvas default
   const near = 0.1;
   const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.set(20, 15, 20);
 
-   const controls = new THREE.OrbitControls(camera, canvas);
+  const controls = new THREE.OrbitControls(camera, canvas);
   controls.autoRotate = true;
   controls.target.set(0, 2, 0);
   controls.minDistance = 10;
   controls.maxDistance = 250;
   controls.minPolarAngle = - 3; // radians
-  controls.minPolarAngle = Math.PI/3;
-controls.maxPolarAngle = Math.PI/3; // radians
+  controls.minPolarAngle = Math.PI/2.3;
+controls.maxPolarAngle = Math.PI/2.3; // radians
   controls.enablePan = false;
   controls.enableZoom = false;
 
-   //controls.minAzimuthAngle = - 2; // radians
+  //controls.minAzimuthAngle = - 2; // radians
   //controls.maxAzimuthAngle = 2; // radians
   controls.update();
+  
+  
+  const scene = new THREE.Scene();
+  
+ 
 
+  
 
-   const scene = new THREE.Scene();
-
-
-
-
-
-   {
+  {
     const skyColor = 0xB1E1FF;  // light blue
     const groundColor = 0xB97A20;  // brownish orange
-    const intensity = 1;
+    const intensity = .5;
     const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
     scene.add(light);
   }
 
-   {
+  {
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, 1.2);
-    light.position.set(5, 8, 7);
+    light.position.set(5, 20, 0);
     scene.add(light);
     scene.add(light.target);
   }
 
-   {
+  {
     const objLoader = new THREE.OBJLoader2();
     objLoader.loadMtl('https://forjos.github.io/arcafe_lib/model/21.mtl', null, (materials) => {
       objLoader.setMaterials(materials);
@@ -66,7 +66,7 @@ controls.maxPolarAngle = Math.PI/3; // radians
     });
   }
 
-   function resizeRendererToDisplaySize(renderer) {
+  function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
@@ -77,43 +77,43 @@ controls.maxPolarAngle = Math.PI/3; // radians
     return needResize;
   }
 
-   function render() {
+  function render() {
 
-     if (resizeRendererToDisplaySize(renderer)) {
+    if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
 
-     renderer.render(scene, camera);
+    renderer.render(scene, camera);
     renderer.setClearColor(0x000000, 0);
+    
+    scene.rotation.y += 0.0015;
 
-     scene.rotation.y += 0.0015;
-
-     requestAnimationFrame(render);
+    requestAnimationFrame(render);
   }
 
-   requestAnimationFrame(render);
+  requestAnimationFrame(render);
 }
 
- main();
+main();
 
 
 
 
- function cake_model() {
+function cake_model() {
 
-   const canvas = document.querySelector('#cake_model');
+  const canvas = document.querySelector('#cake_model');
   const renderer = new THREE.WebGLRenderer({canvas, alpha: true, antialias: true});
-
-   const fov = 45;
+  
+  const fov = 45;
   const aspect = 2;  // the canvas default
   const near = 0.1;
   const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.set(20, 15, 20);
 
-   const controls = new THREE.OrbitControls(camera, canvas);
+  const controls = new THREE.OrbitControls(camera, canvas);
   controls.autoRotate = true;
   controls.target.set(0, 2, 0);
   controls.minDistance = 10;
@@ -123,19 +123,19 @@ controls.maxPolarAngle = Math.PI/3; // radians
   controls.enablePan = false;
   controls.enableZoom = false;
 
-   //controls.minAzimuthAngle = - 2; // radians
+  //controls.minAzimuthAngle = - 2; // radians
   //controls.maxAzimuthAngle = 2; // radians
   controls.update();
+  
+  
+  const scene = new THREE.Scene();
+  
+ 
 
 
-   const scene = new THREE.Scene();
+  
 
-
-
-
-
-
-   {
+  {
     const skyColor = 0xB1E1FF;  // light blue
     const groundColor = 0xB97A20;  // brownish orange
     const intensity = 1.2;
@@ -143,7 +143,7 @@ controls.maxPolarAngle = Math.PI/3; // radians
     scene.add(light);
   }
 
-   {
+  {
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, 1.2);
@@ -152,7 +152,7 @@ controls.maxPolarAngle = Math.PI/3; // radians
     scene.add(light.target);
   }
 
-   {
+  {
     const objLoader = new THREE.OBJLoader2();
     objLoader.loadMtl('https://forjos.github.io/arcafe_lib/model/pizza3.mtl', null, (materials) => {
       objLoader.setMaterials(materials);
@@ -163,7 +163,7 @@ controls.maxPolarAngle = Math.PI/3; // radians
     });
   }
 
-   function resizeRendererToDisplaySize(renderer) {
+  function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
     const width = canvas.clientWidth;
     const height = canvas.clientHeight;
@@ -174,35 +174,35 @@ controls.maxPolarAngle = Math.PI/3; // radians
     return needResize;
   }
 
-   function render() {
+  function render() {
 
-     if (resizeRendererToDisplaySize(renderer)) {
+    if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
 
-     renderer.render(scene, camera);
+    renderer.render(scene, camera);
     renderer.setClearColor(0x000000, 0);
+    
+    scene.rotation.y += 0.0015;
 
-     scene.rotation.y += 0.0015;
-
-     requestAnimationFrame(render);
+    requestAnimationFrame(render);
   }
 
-   requestAnimationFrame(render);
+  requestAnimationFrame(render);
 }
 
 
- cake_model();
+cake_model();
 
 
- $(document).ready(function() {
+$(document).ready(function() {
     $('.close_box_model').click(function(event){
 	$('.box_model').fadeOut('');
 });
 
- $('.btn_box_model').click(function(event){
+$('.btn_box_model').click(function(event){
 	$('.box_model').fadeIn('');
 });
 });
