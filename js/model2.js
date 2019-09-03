0f0c67ce49b95b782cc01051e3807903614f229d
forjos <forjos@mail.ru>
2019-09-04 02:39:22 +0700

    Update model2.js

--- js/model2.js
+++ js/model2.js
@@ -22,8 +22,8 @@ function main() {
   controls.minDistance = 10;
   controls.maxDistance = 250;
   controls.minPolarAngle = - 3; // radians
-  controls.minPolarAngle = Math.PI/2.5;
-controls.maxPolarAngle = Math.PI/2.5; // radians
+  controls.minPolarAngle = Math.PI/2.9;
+controls.maxPolarAngle = Math.PI/2.9; // radians
   controls.enablePan = false;
   controls.enableZoom = false;
 
